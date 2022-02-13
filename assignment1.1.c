bbdb40cadecc15ffcd3f244b8ef0292c2237a9ea
Shrestha8 <98517771+Shrestha8@users.noreply.github.com>
2022-02-13 14:51:10 +0530

    Update assignment1.1.c

--- assignment1.1.c
+++ assignment1.1.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    char name,branch,hobbies;
+    char name[50],branch[50],hobbies[50];
     int regd;
-    printf("Enter your Name:","\n");
+    printf("Enter your Name: \n");
     scanf("%s",&name);
-    printf("Enter your Branch:","\n");
+    printf("Enter your Branch:\n");
     scanf("%s",&branch);
-    printf("Enter your Registration Number(last three digit):","\n");
+    printf("Enter your Registration Number(last three digit):\n");
     scanf("%d",&regd);
-    printf("Enter your Hobbies:","\n");
+    printf("Enter your Hobbies:\n");
     scanf("%s",&hobbies);
     return 0;
 }
